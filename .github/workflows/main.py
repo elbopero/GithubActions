5fb2c7e3d16a00e1490f0964baaed5d8e961d48e
Aman Boparai <88054031+elbopero@users.noreply.github.com>
2024-08-20 20:55:54 +0200

    Delete .github/workflows/main.py

--- .github/workflows/main.py
+++
@@ -1,9 +0,0 @@
-def isNumberOddOrEven(number):
-    if (number % 2) == 0:
-        print("Number is even.")
-    else:
-        print("Number is odd.")
-
-input_number = int(input("Enter number to check:\n"))
-
-isNumberOddOrEven(input_number)
