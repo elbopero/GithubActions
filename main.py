70587df83c265db29425ef2a606424e14c838bf9
Aman <amanboparai@live.no>
2024-08-21 00:45:15 +0200

    changed numbers

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def isNumberOddOrEven(number):
     if (number % 2) == 0:
-        print("Number is even.")
+        print(f"Number {number} is even.")
     else:
-        print("Number is odd.")
+        print(f"Number {number} is odd.")
 
 
-input_number = int(input("Enter number to check: "))
-isNumberOddOrEven(input_number)
+isNumberOddOrEven(10)
+isNumberOddOrEven(15)
